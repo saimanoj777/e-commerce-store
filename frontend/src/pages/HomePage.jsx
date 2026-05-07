812d309c9f8e7baf36bce8bc5b2a9524aa44d998
saimanoj777 <saimanoj2327@gmail.com>
2026-05-07 11:52:42 +0530

    final draft

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ const categories = [
     { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
     { href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
     { href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
-    { href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
+    { href: "/glasses", name: "Glasses", imageUrl: "/glasses.jpg" },
     { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
     { href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
     { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
